c47ffa0716cc80ac370c8f586b1d027ee0a0d0b4
Daniel Carlsson <daniel.carlsson@hemsida24.se>
2017-05-01 20:56:25 +0200

    update

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+# Install Plug vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Copy the .vimrc to the home folder.
 cp .vimrc ~
-mv .vim ~
